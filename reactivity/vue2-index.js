fc65501d54093b1d0c214517e94cded5afe4e7cb
luhaifeng <lhf222458@ncarzone.com>
2021-10-19 07:26:14 +0800

    添加definePrototype练习

---
+++ reactivity/vue2-index.js
@@ -0,0 +1,34 @@
+const obj = { foo: 123 }
+convert(obj)
+
+obj.foo
+obj.foo = 222
+obj.foo
+
+// 判断传入的对象是否符合预期
+function isObject (obj) {
+  return typeof(obj) === 'object'
+        && !Array.isArray(obj)
+        && obj !== null
+        && obj !== undefined
+}
+
+function convert(obj) {
+  if (!isObject(obj)) throw new TypeError('请传入对象')
+
+  Object.keys(obj).forEach(key => {
+    // 因为obj[key]会被使用3次，所以抽成变量了
+    let objVal = obj[key]
+    Object.defineProperty(obj, key, {
+      get() {
+        console.log(`getting key "${key}": ${objVal}`)
+        return objVal
+      },
+
+      set(newVal) {
+        console.log(`setting key "${key}" to: ${newVal}`)
+        objVal = newVal
+      }
+    })
+  })
+}
